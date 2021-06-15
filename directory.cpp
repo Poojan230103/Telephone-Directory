20d598053246b5a7c23c4a6e7768294fb4386380
Poojan <spoojan2003@gmail.com>
2021-06-15 10:05:30 +0530

    Made changes in delete_record function & driver code

--- directory.cpp
+++ directory.cpp
@@ -28,14 +28,35 @@ class directory{
     //  Function to delete the record from the directory.
 };
 
+bool check_digits(char* number)
+{
+    if(strlen(number)==10)
+    {
+        return true;
+    }
+    else if(strlen(number)<10)
+    {
+        cout<<"\nPhone number cannot be less than 10 digits!"<<endl;
+    }
+    else{
+        cout<<"\nPhone number cannot exceed 10 digits!"<<endl;
+    }
+        return false;
+}
+
 void directory::enter_record()
 {
     //  Taking input from the user.
     cout<<"Enter the name: ";
     cin>>this->name;
+
+    do{
     cout<<"\nEnter the phone number: ";
     cin>>this->telephone;
-    cout<<"\nEnter the email ID: ";
+    }
+    while(check_digits(this->telephone) == false);
+
+    cout<<"\nEnter the email address: ";
     cin>>this->email;
     cout<<"\n";
 
@@ -46,12 +67,12 @@ void directory::enter_record()
 
 void directory::display()
 {
-    cout<<"\n\tName\tTelephone Number\tEmail ID\n";
+    cout<<"\n\tName\tTelephone Number\tEmail address\n";
     cout<<"--------------------------------------------------------------------"<<endl;
     fstream file("directory.dat",ios::in|ios::app|ios::binary);
     while(file.read((char*)this,sizeof(*this)))   // input data from file
     {
-    cout<<"\t"<<this->name<<"\t\t"<<this->telephone<<"\t"<<this->email<<endl;
+    cout<<"\t"<<this->name<<"\t"<<this->telephone<<"\t"<<this->email<<endl;
     }
     file.close();
 }
@@ -65,9 +86,10 @@ void directory::search_name(char* number)
         file.read((char*)this,sizeof(*this));
         if(strcmp(this->telephone,number)==0)
         {
-            cout<<"Contact found! "<<endl;
-            cout<<"Name\tTelephone Number\tEmail ID\n";
-            cout<<this->name<<"/t"<<this->telephone<<"/t"<<this->email<<endl;
+            cout<<"\nContact found! "<<endl;
+            cout<<"\nName\tTelephone Number\tEmail ID\n\n";
+            cout<<this->name<<"\t"<<this->telephone<<"\t"<<this->email<<endl;
+            cout<<endl;
             flag = true;
             break;
         }
@@ -88,9 +110,10 @@ void directory::search_number(char* name)
         file.read((char*)this,sizeof(*this));
         if(strcmp(this->name,name)==0)
         {
-            cout<<"Contact found! "<<endl;
-            cout<<"Name\tTelephone Number\tEmail ID"<<endl;
+            cout<<"\nContact found! "<<endl;
+            cout<<"\nName\tTelephone Number\tEmail ID\n"<<endl;
             cout<<this->name<<"\t"<<this->telephone<<"\t"<<this->email<<endl;
+            cout<<endl;
             flag = true;
             break;
         }
@@ -120,34 +143,44 @@ void directory::update_number(char* name)
             dir.write((char*)this,sizeof(*this));
             dir.close();
             flag = true;
-            cout<<"Data updated successfully."<<endl;
+            cout<<"\nDetails updated successfully."<<endl;
         }
         cnt++;
     }
     if(flag == false)
     {
-        cout<<"Data not found!"<<endl;
+        cout<<"\nDetails not found!"<<endl;
     }
 }
 
 void directory::delete_record(char *name)
 {
+    bool flag = false;
     fstream file("directory.dat",ios::in|ios::out|ios::app|ios::binary);
     fstream new_file;
     new_file.open("dummy.dat",ios::out|ios::app|ios::binary);
     while(file.read((char*)this,sizeof(*this)))
+
+        if(strcmp(this->name,name)==0)
     {
+            flag = true;
+    }
         if(strcmp(this->name,name))
         {
             new_file.write((char*)this,sizeof(*this));
         }        
-    }
         file.close();
         new_file.close();
         remove("directory.dat");
         rename("dummy.dat","directory.dat");
         // new_file.close();
+        cout<<"\nContact deleted successfully."<<endl;
+        if(flag == false)
+        {
+            cout<<"\nContact not found!"<<endl;
+        }
 }
+
 
 int main()
 {
@@ -155,16 +188,18 @@ int main()
     int choice=1;
     char name[20],phone[11];
 
-    while(choice != 7)
+    while(choice != 8)
     {
+        cout<<endl;
         cout<<"FILE MENU"<<endl;
-        cout<<"Enter 1 to add a record"<<endl;
-        cout<<"Enter 2 to display the directory"<<endl;
-        cout<<"Enter 3 to search the details of a person"<<endl;
-        cout<<"Enter 4 to search the name and email address"<<endl;  // takes phone number as input and based on it searches the details of the person.
-        cout<<"Enter 5 to update the data"<<endl;
-        cout<<"Enter 6 to erase the directory"<<endl;
-        cout<<"Enter 7 to EXIT"<<endl;
+        cout<<"1. Add new record"<<endl;
+        cout<<"2. Display the directory"<<endl;
+        cout<<"3. Search the details of a person"<<endl;
+        cout<<"4. Search the name and email address"<<endl;  // takes phone number as input and based on it searches the details of the person.
+        cout<<"5. Update the Details"<<endl;
+        cout<<"6. Delete a contact "<<endl;
+        cout<<"7. Erase all contact from the directory"<<endl;
+        cout<<"8. EXIT"<<endl;
         cout<<"Enter your choice: ";
         cin>>choice;
 
@@ -192,20 +227,20 @@ int main()
             direc.update_number(name);
             break;
             case 6:
+            cout<<"Enter the name of the person: "<<endl;
+            cin>>name;
+            direc.delete_record(name);
+            break;
+            case 7:
             remove("directory.dat");
             cout<<"Directory erased successfully."<<endl;
             break;
-            case 7:
+            case 8:
             break;
             default:
             cout<<"Invalid choice! Enter a valid choice: "<<endl;
             break;
         }
     }
-    cout<<"delete"<<endl;
-    cin>>name;
-    direc.delete_record(name);
-    cout<<"print"<<endl;
-    direc.display();
     return 0;
 }
